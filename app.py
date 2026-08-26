99fc6cb0a0cb0aed36a51067aec4beb9fde57ac5
Md Mehedi Khan Maruf <mkmaruf1616@gmail.com>
2026-08-26 15:56:36 +0600

    Fix spreadsheet URL in Google Sheets connection

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1Pk94c2vqopKnEU2nc8Dv0
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 try:
-    df = conn.read(spreadsheet=https://docs.google.com/spreadsheets/d/1Pk94c2vqopKnEU2nc8Dv0aW_z0_9A_TKbFixIReMSL8/edit?gid=1659856766#gid=1659856766, worksheet="Sheet1", ttl=0)
+    df = conn.read(spreadsheet= "https://docs.google.com/spreadsheets/d/1Pk94c2vqopKnEU2nc8Dv0aW_z0_9A_TKbFixIReMSL8/edit?", worksheet="Sheet1", ttl=0)
     df["Date"] = pd.to_datetime(df["Date"]).dt.date
     df["id"] = df["id"].astype(int)
 except Exception as e:
