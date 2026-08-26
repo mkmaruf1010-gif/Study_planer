181ee74c3dc1233d11d5af58061026ac9fe0def5
Md Mehedi Khan Maruf <mkmaruf1616@gmail.com>
2026-08-26 15:46:02 +0600

    Update Google Sheet URL in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="B.Sc. Geography Study Planner", layout="wide")
 st.title("B.Sc. Geography Study Planner")
 
 # Set your Google Sheet URL
-SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1Pk94c2vqopKnEU2nc8Dv0aW_z0_9A_TKbFixIReMSL8/edit?gid=1659856766#gid=1659856766"
+SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1Pk94c2vqopKnEU2nc8Dv0aW_z0_9A_TKbFixIReMSL8/edit"
 
 # Initialize connection (reads automatically from st.secrets)
 conn = st.connection("gsheets", type=GSheetsConnection)
